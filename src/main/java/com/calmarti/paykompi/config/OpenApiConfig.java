a35679486e7ab24007e2a8dc026d7b67a45598ed
Ivan Martinez <calmartivan@gmail.com>
2026-03-03 11:07:09 +0100

    add springdoc-openapi-starter-webmvc-ui - v3.0.1 (openapi) and OpenApiConfig class

---
+++ src/main/java/com/calmarti/paykompi/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.calmarti.paykompi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//http://localhost:8080/swagger-ui/index.html
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PayKompi RESTful API")
+                        .version("1.0")
+                        .description("REST API documentation"));
+    }
+}
